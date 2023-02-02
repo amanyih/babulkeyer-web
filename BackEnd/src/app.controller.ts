5accadcabb243c8f539c677a5b85be25271c79f1
Merwan-J <merwanj71@gmail.com>
2023-02-02 18:13:38 +0300

    fixed boilerplate

--- BackEnd/src/app.controller.ts
+++ BackEnd/src/app.controller.ts
@@ -5,26 +5,4 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post('/add')
-  add(@Body() numbers: { first: number; second: number }) {
-    const result: number = this.appService.add(numbers.first, numbers.second);
-    return { result };
-  }
-
-  @Post('/sub')
-  sub(@Body() numbers: { first: number; second: number }) {
-    const result: number = this.appService.sub(numbers.first, numbers.second);
-    return { result };
-  }
-
-  @Post('/div')
-  div(@Body() numbers: { first: number; second: number }) {
-    const result: number = this.appService.div(numbers.first, numbers.second);
-    return { result };
-  }
-  @Post('/mult')
-  mult(@Body() numbers: { first: number; second: number }) {
-    const result: number = this.appService.mult(numbers.first, numbers.second);
-    return { result };
-  }
 }
--- BackEnd/src/app.service.ts
+++ BackEnd/src/app.service.ts
@@ -2,16 +2,5 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  add(a: number, b: number): number {
-    return a + b;
-  }
-  sub(a: number, b: number): number {
-    return a - b;
-  }
-  mult(a: number, b: number): number {
-    return a * b;
-  }
-  div(a: number, b: number): number {
-    return a / b;
-  }
+  
 }
